a6af0e3583964fc702ec1fd77a01c342110a49bc
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2024-09-18 15:54:08 +0600

    navbar user name and logout button add

--- src/Layouts/Navabr.jsx
+++ src/Layouts/Navabr.jsx
@@ -1,8 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AsideContext } from "../context";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const { opoenAside, setOpenAside } = useContext(AsideContext);
+  const [user, setUser] = useState({});
+  const [userInfo, setUserInfo] = useState(false);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const getUser = localStorage.getItem("user");
+    const getUserParse = JSON.parse(getUser).user;
+    setUser(getUserParse);
+  }, []);
+
+  const handleClick = () => {
+    localStorage.removeItem("user");
+    navigate("/login");
+  };
+
   return (
     <nav className="flex justify-between md:flex-none bg-gray-800 py-4 border-b-[1px] border-gray-700">
       <button
@@ -11,10 +26,24 @@ export default function Navbar() {
       >
         <i className="fa-solid fa-bars"></i>
       </button>
-      <div className="flex justify-end md:mr-10 mr-4 text-white xl:container mx-auto">
-        <h1 className="bg-[#0C4A6E] flex justify-center items-center text-center w-10 h-10 rounded-full cursor-pointer">
-          N
+      <div className="flex justify-end md:mr-10 mr-4 text-white xl:container mx-auto relative">
+        <h1
+          onMouseEnter={() => setUserInfo(true)}
+          className="bg-[#0C4A6E] flex justify-center items-center text-center w-10 h-10 rounded-full cursor-pointer"
+        >
+          {user?.name?.charAt(0)}
         </h1>
+        {userInfo && (
+          <div
+            onMouseLeave={() => setUserInfo(false)}
+            className="p-2 absolute top-[38px] z-10 bg-gray-800 rounded flex justify-center items-start flex-col"
+          >
+            <h1>{user?.name}</h1>
+            <h1 onClick={handleClick} className="cursor-pointer">
+              Log Out
+            </h1>
+          </div>
+        )}
       </div>
     </nav>
   );
